4116afeb9ff8a385477318fae56503891a2a7db3
İlkay ŞENER <52975475+ilkaysener@users.noreply.github.com>
2022-04-06 15:48:51 +0300

    Update Start.js
    In the original version of the code, it was not possible to copy from card pages opened from a list with the Ctrl+C option blocked. The new version of the code only targets list pages with a control addin.

--- JsScript/Start.js
+++ JsScript/Start.js
@@ -1,5 +1,11 @@
 $(parent.document).ready(function() {
-    parent.document.body.oncopy = function(e) {
-        e.preventDefault();
+    parent.document.body.oncopy = function(e) {   
+        const pageIds = ['22', '5052'];
+        let searchParams = new URLSearchParams(parent.parent.document.location.search);
+        if (pageIds.includes(searchParams.get('page')))
+        {
+            console.log('Ctrl+C is blocked.');
+            e.preventDefault();
+        }
     }
 });
