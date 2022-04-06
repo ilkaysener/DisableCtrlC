9f8a8e81cd6f03d69fd7d9d64958281143ffeab0
İlkay ŞENER <52975475+ilkaysener@users.noreply.github.com>
2022-04-06 15:56:47 +0300

    No Need JQuery

--- JsScript/Start.js
+++ JsScript/Start.js
@@ -1,11 +1,9 @@
-$(parent.document).ready(function() {
-    parent.document.body.oncopy = function(e) {   
-        const pageIds = ['22', '5052'];
-        let searchParams = new URLSearchParams(parent.parent.document.location.search);
-        if (pageIds.includes(searchParams.get('page')))
-        {
-            console.log('Ctrl+C is blocked.');
-            e.preventDefault();
-        }
+parent.document.body.oncopy = function(e) {   
+    const pageIds = ['22', '5052'];
+    let searchParams = new URLSearchParams(parent.parent.document.location.search);
+    if (pageIds.includes(searchParams.get('page')))
+    {
+        console.log('Ctrl+C is blocked.');
+        e.preventDefault();
     }
-});
+}
